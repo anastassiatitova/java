741780458fe5644311ad99210e868c477f0b9a55
Anstassia Titova <garderiebetty@yahoo.ca>
2021-01-07 15:55:56 -0500

    update 01072021

--- Chapter14/src/domain/Test.java
+++ Chapter14/src/domain/Test.java
@@ -1,20 +1,44 @@
 package domain;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class Test extends Application {
-	public Test() {
-		System.out.println("Test constructor is invoked");
-	}
 
 	@Override
 	public void start(Stage primaryStage) {
-		System.out.println("Start method is invoked");
+		FlowPane pane = new FlowPane();
+		Arc arc = new Arc(70, 70, 50, 50, 0, 180);
+		arc.setType(ArcType.OPEN);
+		arc.setFill(null);
+		arc.setStroke(Color.BLACK);
+		Arc arc1 = new Arc(150, 150, 50, 50, 180, 180);
+		arc1.setType(ArcType.ROUND);
+		arc1.setFill(Color.RED);
+		arc1.setStroke(Color.BLACK);
+
+		Polygon poly = new Polygon(20, 40, 30, 50, 40, 90, 90, 10, 10, 30);
+		poly.setFill(Color.GREEN);
+		Polygon poly1 = new Polygon(20, 40, 30, 50, 40, 90, 90, 10, 10, 30);
+
+		pane.getChildren().add(arc);
+		pane.getChildren().add(arc1);
+		pane.getChildren().add(poly);
+		pane.getChildren().add(poly1);
+
+		Scene scene = new Scene(pane, 200, 300);
+		primaryStage.setTitle("Test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
-		System.out.println("launch application");
-		Application.launch(args);
+		launch(args);
 	}
 }
--- Java Basic/src/basic/Exercise.java
+++ Java Basic/src/basic/Exercise.java
@@ -10,20 +10,25 @@ public class Exercise {
 	public static final String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","","thousand","million","billion","trillion","quadrillion","quintillion"};
     
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int z = 0; z < n; z++) {
-            String s = in.next();
-		    String ns = s.replaceFirst("^-?0*", "");
-		    ns = (ns.length()%3==0?"":ns.length()%3==1?"00":"0") + ns;
-		    char[] nc = ns.toCharArray();
-		    String res =  (s.startsWith("-") ? "negative " : "") + (nc.length/3 == 0 ? arr[0]:"" );
-		    for(int i = 0; i < nc.length/3; i++) {
-			    int d1 = nc[i*3]-48, d2 = nc[i*3+1]-48, d3 = nc[i*3+2]-48;
-			    res += " " + (d1>0 ? arr[d1] + " hundred" : "") + " " + (d2+d3>0 ? d2*10+d3<20? arr[d2*10+d3] : arr[20+d2] + (d3>0 ? "-" + arr[d3] : "") : "") + " " + (d1+d2+d3>0 ? arr[nc.length/3-i+29] : "");
-		    }
-		    System.out.println(res.replaceAll("\\s{2,}", " ").trim());
-        }
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        for (int z = 0; z < n; z++) {
+//            String s = in.next();
+//		    String ns = s.replaceFirst("^-?0*", "");
+//		    ns = (ns.length()%3==0?"":ns.length()%3==1?"00":"0") + ns;
+//		    char[] nc = ns.toCharArray();
+//		    String res =  (s.startsWith("-") ? "negative " : "") + (nc.length/3 == 0 ? arr[0]:"" );
+//		    for(int i = 0; i < nc.length/3; i++) {
+//			    int d1 = nc[i*3]-48, d2 = nc[i*3+1]-48, d3 = nc[i*3+2]-48;
+//			    res += " " + (d1>0 ? arr[d1] + " hundred" : "") + " " + (d2+d3>0 ? d2*10+d3<20? arr[d2*10+d3] : arr[20+d2] + (d3>0 ? "-" + arr[d3] : "") : "") + " " + (d1+d2+d3>0 ? arr[nc.length/3-i+29] : "");
+//		    }
+//		    System.out.println(res.replaceAll("\\s{2,}", " ").trim());
+// 
+    	System.out.println( 4 + 20 / (3 - 1) * 2);
+    	int x = 2;
+    	int y = 1;
+    	x *= y + 1;
+    	System.out.println(x);
     }
 }
 	
